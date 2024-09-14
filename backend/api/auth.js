22635b55d28cf41b1a4d2c29720a0e46187900b1
Prakash Kumar <prakashkumar01031985@gmail.com>
2024-09-14 14:12:44 +0530

    vercel

--- backend/api/auth.js
+++ backend/api/auth.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const { body, validationResult } = require('express-validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-const fetchuser = require('../middleware/fetchUser')
+const fetchUser = require('../middleware/fetchUser')
 
 const JWT_SCREAT = process.env.JWTSIGN
 
