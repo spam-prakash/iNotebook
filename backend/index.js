a38a9f17b883f2fc0187bbe7ec305153c9423a7b
Prakash Kumar <prakashkumar01031985@gmail.com>
2024-09-15 03:08:26 +0530

    mmm9

--- backend/index.js
+++ backend/index.js
@@ -12,22 +12,22 @@ connectToMongo()
 //   'http://localhost:3006' // Local development
 // ]
 
-app.use(cors({
-  origin: 'https://inotebook-frontend-murex.vercel.app', // Frontend domain
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true // If you need to include cookies/auth tokens
-}))
+// app.use(cors({
+//   origin: 'https://inotebook-frontend-murex.vercel.app', // Frontend domain
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true // If you need to include cookies/auth tokens
+// }))
 
-app.options('*', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://inotebook-frontend-murex.vercel.app')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-  res.setHeader('Access-Control-Allow-Credentials', 'true')
-  res.status(200).send()
-})
+// app.options('*', (req, res) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'https://inotebook-frontend-murex.vercel.app')
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+//   res.setHeader('Access-Control-Allow-Credentials', 'true')
+//   res.status(200).send()
+// })
 
-// app.use(cors())
+app.use(cors())
 
 // Avilable Routes
 app.use('/api/auth', require('./api/auth'))
