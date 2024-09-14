2a1493294e2df6f6c4b66a15f6af3befb3957c66
Prakash Kumar <prakashkumar01031985@gmail.com>
2024-09-15 02:44:34 +0530

    mmm2

--- backend/index.js
+++ backend/index.js
@@ -7,18 +7,26 @@ const port = 8000
 connectToMongo()
 
 // CORS Configuration
-// const allowedOrigins = [
-//   'https://inotebook-frontend-murex.vercel.app', // Deployed frontend URL
-//   'http://localhost:3006' // Local development
-// ]
+const allowedOrigins = [
+  'https://inotebook-frontend-murex.vercel.app', // Deployed frontend URL
+  'http://localhost:3006' // Local development
+]
 
-// app.use(cors({
-//   origin: '*', // Allow requests from any origin (for testing)
-//   methods: 'GET,POST,PUT,DELETE', // Allowed methods
-//   credentials: true // Allow sending cookies and credentials
-// }))
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true)
+    if (allowedOrigins.indexOf(origin) === -1) {
+      return callback(new Error('CORS policy does not allow this origin'), false)
+    }
+    return callback(null, true)
+  },
+  methods: 'GET,POST,PUT,DELETE',
+  credentials: true // Allow sending cookies and credentials
+}))
 
-app.use(cors())
+
+// app.use(cors())
 
 // Avilable Routes
 app.use('/api/auth', require('./api/auth'))
