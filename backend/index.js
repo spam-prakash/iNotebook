e542a788d65f5409d41e275dfba6914ab3b6e995
Prakash Kumar <prakashkumar01031985@gmail.com>
2024-09-14 13:52:12 +0530

    fk

--- backend/index.js
+++ backend/index.js
@@ -7,24 +7,24 @@ const port = 8000
 connectToMongo()
 
 // CORS Configuration
-const allowedOrigins = [
-  'https://inotebook-frontend-murex.vercel.app', // Deployed frontend URL
-  'http://localhost:3006' // Local development
-]
+// const allowedOrigins = [
+//   'https://inotebook-frontend-murex.vercel.app', // Deployed frontend URL
+//   'http://localhost:3006' // Local development
+// ]
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true) // Allow requests with no origin, like from mobile apps
-    if (allowedOrigins.indexOf(origin) === -1) {
-      return callback(new Error('CORS policy does not allow this origin'), false)
-    }
-    return callback(null, true)
-  },
-  methods: 'GET,POST,PUT,DELETE', // Allowed methods
-  credentials: true // Allow sending cookies and credentials
-}))
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin) return callback(null, true) // Allow requests with no origin, like from mobile apps
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       return callback(new Error('CORS policy does not allow this origin'), false)
+//     }
+//     return callback(null, true)
+//   },
+//   methods: 'GET,POST,PUT,DELETE', // Allowed methods
+//   credentials: true // Allow sending cookies and credentials
+// }))
 
-// app.use(cors())
+app.use(cors())
 
 // Avilable Routes
 app.use('/api/auth', require('./api/auth'))
