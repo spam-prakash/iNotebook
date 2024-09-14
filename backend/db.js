07273777c5fa41b389197441424cb93736eb0595
Prakash Kumar <prakashkumar01031985@gmail.com>
2024-09-14 13:57:42 +0530

    mongouri

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,21 @@
-require('dotenv').config();
-const mongoose=require('mongoose')
-const mongoURI=process.env.MONGOURI
+require('dotenv').config()
+const mongoose = require('mongoose')
+// const mongoURI=process.env.MONGOURI
+// const mongoURI = 'mongodb://127.0.0.1:27017/inotebook'
+const mongoURI = 'mongodb+srv://akash_raushan_:akash12345@cluster0.cjsil.mongodb.net/inotebook'
 // console.log(mongoURI)
 
 const connectToMongo = async () => {
-    try {
-        await mongoose.connect(mongoURI);
-        console.log("Connected to Mongoose Successfully");
-    } catch (error) {
-        console.error("Failed to connect to Mongoose:", error);
-    }
-};
+  try {
+    await mongoose.connect(mongoURI)
+    console.log('Connected to Mongoose Successfully')
+  } catch (error) {
+    console.error('Failed to connect to Mongoose:', error)
+  }
+}
 
 // mongoose.connect(mongoURI).then(()=>{
 //     console.log("Connected")
 // }).catch((err)=>{console.log("Error Occured ",err)})
 
-module.exports=connectToMongo;
+module.exports = connectToMongo
