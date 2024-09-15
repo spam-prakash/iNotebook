616b9bdf72db904684abfc4d75489bda83478d58
Prakash Kumar <prakashkumar01031985@gmail.com>
2024-09-15 20:12:37 +0530

    min fix addnote title

--- frontend/src/components/Addnote.js
+++ frontend/src/components/Addnote.js
@@ -1,5 +1,5 @@
-import React, { useContext, useState } from 'react';
-import noteContext from '../context/notes/NoteContext';
+import React, { useContext, useState } from 'react'
+import noteContext from '../context/notes/NoteContext'
 // import NoteItem from './NoteItem';
 // import AddNote from './Addnote';
 
@@ -18,7 +18,7 @@ const Addnote = (props) => {
 
   const onChange = (e) => {
     setNote({ ...note, [e.target.name]: e.target.value })
-  };
+  }
   return (
     <div className='add mt-[6.5rem]'>
       <h1 className="text-white text-3xl font-['Open_sans'] font-bold mb-2 mx-4">
@@ -33,7 +33,7 @@ const Addnote = (props) => {
             Title
           </label>
           <input
-value={note.title}
+            value={note.title}
             id='title'
             name='title'
             type='text'
@@ -50,13 +50,13 @@ value={note.title}
             Description
           </label>
           <input
-value={note.description}
+            value={note.description}
             id='description'
             name='description'
             className='shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-1 focus:shadow-outline outline-[#0F1729]'
             placeholder='Enter description'
             onChange={onChange}
-           />
+          />
         </div>
         <div className='mb-4'>
           <label
@@ -66,7 +66,7 @@ value={note.description}
             Tag
           </label>
           <input
-value={note.tag}
+            value={note.tag}
             id='tag'
             name='tag'
             type='text'
@@ -79,12 +79,12 @@ value={note.tag}
           <button
             type='button'
             className={`bg-[#FFD252] hover:bg-[#FDC116] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ${
-              note.title.length < 5 || note.description.length < 3
+              note.title.length < 3 || note.description.length < 3
                 ? 'disabled cursor-not-allowed opacity-50'
                 : ''
             }`}
             onClick={handleClick}
-            disabled={note.title.length < 5 || note.description.length < 3}
+            disabled={note.title.length < 3 || note.description.length < 3}
           >
             Add Note
           </button>
@@ -92,6 +92,6 @@ value={note.tag}
       </form>
     </div>
   )
-};
+}
 
 export default Addnote
